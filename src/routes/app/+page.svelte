e3b8f6f5eb431b955c481039ddb9734af8f02fe8
ThmLeon <leon.thumm@t-online.de>
2025-09-13 17:15:42 +0200

    fix: redirect from home always to /events

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,1 +1,10 @@
+<script>
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	onMount(() => {
+		goto('/events', { replaceState: true });
+	});
+</script>
+
 <h1>Hello world</h1>
